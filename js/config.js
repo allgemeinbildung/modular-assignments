ca465e9b7e15cf3144083ab9a38ecb3cfb69f792
Pietro Rossi <LP4PRossi@gibwintra.ch>
2025-07-04 15:28:55 +0200

    Add config file for deployment

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// Rename this file to "config.js" and paste your Google Apps Script Web App URL here.
+// DO NOT commit the "config.js" file to your repository if it's public.
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzbg4K3ZtFgW_jTI359Hi_9DPOsHN_-BliFvZYACz_pN8xQf-NC-1dBAhmI5ONWAMbJ/exec';
